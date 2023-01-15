cc58c2e4776e0d020fe3a3e2f8d9121ec4505d84
nategoodman88 <nategoodman88@gmail.com>
2023-01-15 11:05:40 -0500

    finished arrays

--- app.js
+++ app.js
@@ -17,44 +17,51 @@ async function getMales() {
         const $ = cheerio.load(response.data);
         //Find any element with class rig (the class used by Doe Network's UL of Does) then parse through each child LI, then parse through each child *x* and save that as link to Doe's page. Save h3 and save that as the title of the Doe. Save attributes as attributes of the Doe. Push to previous array for saving
         $(".rig", response.data).each((index, element) => {
-            $(element).children("li").each((index, element) => {
-                //let doeNetworkLink= $(element).children("").text(); -> WIP
+            $(element).children("li").each((value, element) => {
+                $(element).children('a').each((value, aElement) => {
+                let doeNetworkLink = ($(aElement).attr('href')); 
+                //console.log(doeNetworkLink);
                 let maleDoeTitle = $(element).children("h3").text().toString();
                 let maleDoeAttributes= $(element).text().toString();
                 //Replace redundant info
                 maleDoeAttributes = maleDoeAttributes.replace(maleDoeTitle, "")
-                male_doe_arr.push({/*doeNetworkLink,*/maleDoeTitle,maleDoeAttributes});
-                //console.log(male_doe_arr); -> for debugging
+                male_doe_arr.push({doeNetworkLink,maleDoeTitle,maleDoeAttributes});
+                //console.log(male_doe_arr); //-> for debugging
                 return male_doe_arr;
             })
         })
-    }
-    catch(e) {
-        console.error(e);
-    }
+    })
 }
+catch(e) {
+    console.error(e);
+}
+}
+
 
 async function getFemales() {
     try {
-        const response = await axios.get(female_doe_url);
+        const response = await axios.get(male_doe_url);
         const $ = cheerio.load(response.data);
         //Find any element with class rig (the class used by Doe Network's UL of Does) then parse through each child LI, then parse through each child *x* and save that as link to Doe's page. Save h3 and save that as the title of the Doe. Save attributes as attributes of the Doe. Push to previous array for saving
         $(".rig", response.data).each((index, element) => {
-            $(element).children("li").each((index, element) => {
-                //let doeNetworkLink = $(element).children("").text(); -> WIP
+            $(element).children("li").each((value, element) => {
+                $(element).children('a').each((value, aElement) => {
+                let doeNetworkLink = ($(aElement).attr('href')); 
+                //console.log(doeNetworkLink);
                 let femaleDoeTitle = $(element).children("h3").text().toString();
                 let femaleDoeAttributes= $(element).text().toString();
                 //Replace redundant info
                 femaleDoeAttributes = femaleDoeAttributes.replace(femaleDoeTitle, "")
-                female_doe_arr.push({/*doeNetworkLink,*/femaleDoeTitle,femaleDoeAttributes});
-                //console.log(female_doe_arr); -> for debugging
-                return female_doe_arr;
+                female_doe_arr.push({doeNetworkLink,femaleDoeTitle,femaleDoeAttributes});
+                //console.log(male_doe_arr); //-> for debugging
+                return male_doe_arr;
             })
         })
-    }
-    catch(e) {
-        console.error(e);
-    }
+    })
+}
+catch(e) {
+    console.error(e);
+}
 }
 function parseData() {
     getMales();
